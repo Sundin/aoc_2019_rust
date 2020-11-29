25254fffb4fd8c8f630ca5f668f11ec58ae84071
Gustav Sundin <sundin666@gmail.com>
2020-11-29 19:30:28 +0100

    Solution day 1 part 1

--- src/lib.rs
+++ src/lib.rs
@@ -31,13 +31,33 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     // ? will return the error rather than panic
     let contents = fs::read_to_string(config.filename)?;
 
-    println!("Hello Rust");
+    let total_fuel = get_total_fuel(&contents);
+    println!("Answer for day 1 part 1: {}", total_fuel);
 
     Ok(())
 }
 
+fn get_total_fuel(contents: &str) -> i32 {
+    let mut fuel = 0;
+    for line in contents.lines() {
+        let line: i32 = match line
+            .trim()
+            .parse() {
+                Ok(num) => num,
+                // _ = match all Err values (place for proper error handling)
+                Err(_) => {
+                    println!("Please type a number!");
+                    continue;
+                }
+            };
+        fuel = fuel + get_fuel_requirements(line);
+    }
+    fuel
+}
+
 fn get_fuel_requirements(mass: i32) -> i32 {
-    0
+    let m2 = mass / 3;
+    m2 - 2
 }
 
 
@@ -47,6 +67,9 @@ mod tests {
 
     #[test]
     fn fuel_requirements() {
-        assert_eq!(0, get_fuel_requirements(0));
+        assert_eq!(2, get_fuel_requirements(12));
+        assert_eq!(2, get_fuel_requirements(14));
+        assert_eq!(654, get_fuel_requirements(1969));
+        assert_eq!(33583, get_fuel_requirements(100756));
     }
 }
