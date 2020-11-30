dd306451dbe6ecdb3e229b9c6caa16f4cabe1ee5
Gustav Sundin <sundin666@gmail.com>
2020-11-30 09:11:23 +0100

    Multiplication

--- src/intcode.rs
+++ src/intcode.rs
@@ -17,7 +17,13 @@ fn get_output(intcodes: &mut[i32], pointer: usize) -> i32 {
             println!("{} + {} = {} => pos {}", parameter1, parameter2, parameter1+parameter2, destination);
             intcodes[destination as usize] = parameter1 + parameter2;
         },
-        2 => println!("two"),
+        2 => {
+            let parameter1 = intcodes[intcodes[pointer+1] as usize];
+            let parameter2 = intcodes[intcodes[pointer+2] as usize];
+            let destination = intcodes[pointer+3];
+            println!("{} * {} = {} => pos {}", parameter1, parameter2, parameter1*parameter2, destination);
+            intcodes[destination as usize] = parameter1 * parameter2;
+        },
         99 => {
             println!("Finished!");
             return intcodes[0];
@@ -36,5 +42,16 @@ mod tests {
     #[test]
     fn test_addition() {
         assert_eq!(2, get_output(&mut [1,0,0,0,99], 0));
+    }
+
+    #[test]
+    fn test_multiplication() {
+        assert_eq!(2, get_output(&mut [2,3,0,3,99], 0));
+        assert_eq!(2, get_output(&mut [2,4,4,5,99,0], 0))
+    }
+
+    #[test]
+    fn test_multiple_rounds() {
+        assert_eq!(30, get_output(&mut [1,1,1,4,99,5,6,0,99], 0));
     }
 }
